e5db496ffc0ec222e761f65c75e72960669d6760
Harsh Singh <webwithfusion@gmail.com>
2025-09-20 03:34:53 +0530

    commit
    added comments to all .py files

--- load_data.py
+++ load_data.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+# Load the dataset
 df = pd.read_csv("data/sample_data.csv")
 
 print("First 5 rows of the dataset:")
 print(df.head())
 
+# Show dataset info
 print("\nDataset info:")
 print(df.info())
